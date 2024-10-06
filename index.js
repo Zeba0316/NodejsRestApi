d86c2a937ea3005a67ea31c2e60dde999bb9c784
zeba <zebakhan0316@gmail.com>
2024-10-06 21:21:27 +0530

    .env.sample created and basic server structure created

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+// imports
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const port=process.env.PORT;
 
+// express app initailization
+const app = express();
+
+// middlewares
+app.use(cors());
+app.use(bodyParser.json());
+
+// listener
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
