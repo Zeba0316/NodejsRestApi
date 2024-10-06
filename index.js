e467d1a92cde47162ece5c08023022269de35d1c
zeba <zebakhan0316@gmail.com>
2024-10-06 21:31:38 +0530

    mongodb connection established

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 require("dotenv").config();
-const port=process.env.PORT;
+const port = process.env.PORT;
 
 // express app initailization
 const app = express();
@@ -13,7 +13,17 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+// mongodb connection
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("error in connecting to mongodb", err);
+  });
+
 // listener
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-})
+  console.log(`Listening on port ${port}`);
+});
